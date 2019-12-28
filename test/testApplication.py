f40fb30b28ca75bffbeac4999b147f0f54d9e986
Nicholas Wright <dozedoffagain@gmail.com>
2019-12-28 18:06:43 +0100

    Simple test for login and read version

---
+++ test/testApplication.py
@@ -0,0 +1,13 @@
+import unittest
+from qbittorrentapi import Client
+
+class ApplicationTests(unittest.TestCase):
+    def setUp(self):
+        self.client = Client(host='192.168.42.10:8080', VERIFY_WEBUI_CERTIFICATE=False)
+        self.client.auth_log_in('test', '12345678')
+
+    def test_login(self):
+        self.assertEqual(self.client.application.version, 'v4.2.1')
+
+if __name__ == '__main__':
+    unittest.main()
