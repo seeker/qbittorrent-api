462d6c318e06990e934904558f43b346aa9d54dc
Nicholas Wright <dozedoffagain@gmail.com>
2019-12-29 21:46:08 +0100

    Changed test user to vagrant_test

--- test/testApplication.py
+++ test/testApplication.py
@@ -4,7 +4,7 @@ from qbittorrentapi import Client
 class ApplicationTests(unittest.TestCase):
     def setUp(self):
         self.client = Client(host='192.168.42.10:8080', VERIFY_WEBUI_CERTIFICATE=False)
-        self.client.auth_log_in('test', '12345678')
+        self.client.auth_log_in('vagrant_test', '12345678')
 
     def test_login(self):
         self.assertEqual(self.client.application.version, 'v4.2.1')
